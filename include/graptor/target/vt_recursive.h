36c98ebc4af0128c1c2cd629274128ce39d1bd7f
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2021-11-23 16:19:48 +0000

    add bitwise_xor

--- include/graptor/target/vt_recursive.h
+++ include/graptor/target/vt_recursive.h
@@ -277,6 +277,10 @@ struct vt_recursive {
 	return type { lo_half_traits::bitwise_or( l.a, r.a ),
 		hi_half_traits::bitwise_or( l.b, r.b ) };
     }
+    static type bitwise_xor( type l, type r ) {
+	return type { lo_half_traits::bitwise_xor( l.a, r.a ),
+		hi_half_traits::bitwise_xor( l.b, r.b ) };
+    }
     static type bitwise_invert( type l ) {
 	return type { lo_half_traits::bitwise_invert( l.a ),
 		hi_half_traits::bitwise_invert( l.b ) };
