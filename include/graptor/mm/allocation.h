8469a3bdc22a525022807f6af908c276ee3af331
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2021-11-29 23:27:18 +0000

    add boolean flags on memory mapped and aligned nature

--- include/graptor/mm/allocation.h
+++ include/graptor/mm/allocation.h
@@ -10,13 +10,20 @@ namespace mm {
 
 class allocation {
 public:
-    allocation( intptr_t ptr, size_t size ) : m_ptr( ptr ), m_size( size ) { }
+    allocation( intptr_t ptr, size_t size,
+		bool mapped = false, bool aligned = false )
+	: m_ptr( ptr ), m_size( size ),
+	  m_mapped( mapped ), m_aligned( aligned ) { }
 
     intptr_t ptr() const { return m_ptr; }
     size_t size() const { return m_size; }
+    bool is_mapped() const { return m_mapped; }
+    bool is_aligned() const { return m_aligned; }
 private:
-    intptr_t m_ptr;
-    size_t m_size;
+    intptr_t m_ptr; //!< address of the allocation
+    size_t m_size;  //!< size of the allocation
+    bool m_mapped;  //!< is this mmap/munmap (true), or malloc/free (false)
+    bool m_aligned; //!< for mmap'ed data, if ptr was aligned to small page
 };
     
 } // namespace mm
