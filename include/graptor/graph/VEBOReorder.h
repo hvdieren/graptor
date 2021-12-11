55fb53af8f36d909c97138900a87125c743b2fbd
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2021-12-11 21:49:43 +0000

    tentative improvements for case P>1, pmul>1 (Graptor)

--- include/graptor/graph/VEBOReorder.h
+++ include/graptor/graph/VEBOReorder.h
@@ -310,6 +310,8 @@ private:
 
 	    return pmin;
 	}
+
+	
     };
 
     VID place_vertices_simd( int P, int gP, int vP,
@@ -488,6 +490,24 @@ private:
 
 	    std::fill( &a[0], &a[n], 0 ); // important: a aliases last
 	} else { // P > 1
+	    // Identify the lowest degree for which
+	    // ( #vertices at degree <= d ) <= P*(pmul-1)
+	    // From d_crit to lower degrees, we need to try to obtain
+	    // partitions with a multiple of pmul vertices. We need to
+	    // start doing this from d_crit onwards, and it will be possible
+	    // to obtain this, except perhaps for the final partition.
+	    VID nv = 0;
+	    VID d_crit = dmax;
+	    for( VID d=0; d <= dmax; ++d ) {
+		nv += histo[d];
+		if( nv >= P * ( pmul - 1 ) ) {
+		    d_crit = d;
+		    break;
+		}
+	    }
+	    std::cerr << "VEBO: critical d: " << d_crit << "\n";
+
+	    // Assign vertices to partitions
 	    VID d = dmax;
 	    VID vlast = ~(VID)0;
 	    do {
@@ -510,8 +530,7 @@ private:
 		// where vertices are placed.
 		std::copy( &u[0], &u[P], &du[0] );
 
-		VID j = 0;
-		for( ; j < k; ++j ) {
+		for( VID j=0; j < k; ++j ) {
 		    // place vertex in least-loaded partition, while tracking
 		    // statistics on the spread of the loads
 		    load.place( d );
@@ -524,20 +543,37 @@ private:
 		    //       As such, we can safely place K vertices at once.
 		    if( load.Delta <= d && ((k-j-1) % P) == 0 ) {
 			++j;
+			// Remaining vertices are distributed equally over all
+			// partitions
+			// The scale-free property implies that most of the
+			// vertices are handled by this loop
+			if( j < k ) {
+			    int r = (k-j) / P;
+			    for( int p=0; p < P; ++p ) {
+				u[p] += r;
+				w[p] += r * d;
+			    }
+			}
 			break;
 		    }
 		}
 
-		// Remaining vertices are distributed equally over all
-		// partitions
-		// The scale-free property implies that most of the vertices
-		// are handled by this loop
-		if( j < k ) {
-		    int r = (k-j) / P;
-		    for( int p=0; p < P; ++p ) {
-			u[p] += r;
-			w[p] += r * d;
+		// Rebalance such that partitions hold a multiple of pmul
+		// vertices.
+		// Assumes P > 1.
+		if( pmul > 1 && d <= d_crit ) {
+		    VID mv = std::min( u[0] % pmul, u[0] - du[0] );
+		    u[0] -= mv;
+		    w[0] -= mv * d;
+		    for( unsigned p=1; p < P; ++p ) {
+			u[p] += mv;
+			w[p] += mv * d;
+			mv = std::min( u[p] % pmul, u[p] - du[p] );
+			u[p] -= mv;
+			w[p] -= mv * d;
 		    }
+		    u[P-1] += mv;
+		    w[P-1] += mv * d;
 		}
 
 		// Now determine which vertices are placed in each partition.
@@ -548,7 +584,6 @@ private:
 		else
 		    vlast = place_vertices_simd( P, gP, vP, first, next, a,
 						 d, u, du );
-		// vlast = last[d]; -- but last reused for a
 	    } while( d-- > 0 ); // loops over d == 0 as well
 	}
 
@@ -572,12 +607,14 @@ private:
 	VID *s = du; // reuse array
 	VID nwpad = n;
 	if( pmul > 1 && P > 1 ) {
+#if 0
 	    // Shift vertices through to next partition to have multiple of pmul
 	    s[0] = 0;
 	    VID mask = pmul-1;
 	    assert( (pmul & (pmul - 1)) == 0 );
 	    if( u[0] > pmul ) {
 		VID carry = u[0] & mask;
+		assert( carry == 0 );
 		u[0] -= carry;
 		u[1] += carry;
 		part.inuse_as_array()[0] = u[0];
@@ -593,6 +630,7 @@ private:
 		if( p < P-1 ) {
 		    if( u[p] > pmul ) {
 			VID carry = u[p] & mask;
+			assert( carry == 0 );
 			u[p] -= carry;
 			u[p+1] += carry;
 			part.inuse_as_array()[p] = u[p];
@@ -618,6 +656,42 @@ private:
 	    // Aggregate values
 	    part.as_array()[P] = nwpad;
 	    part.compute_starts_inuse(); // same as s[]
+#else
+	    // Already shifted vertices through. Calculate and record
+	    // vertices/partition (including padding) and inuse-vertices
+	    // (not including padding)
+	    VID mask = pmul-1;
+	    assert( (pmul & (pmul - 1)) == 0 );
+	    s[0] = 0;
+	    for( int p=0; p < P; ++p ) {
+		if( p < P-1 ) {
+		    if( u[p] >= pmul ) {
+			part.inuse_as_array()[p] = u[p];
+		    } else {
+			// at least a multiple of pmul -> add padding
+			nwpad += pmul - u[p];
+			part.inuse_as_array()[p] = u[p];
+			u[p] = pmul;
+		    }
+		} else {
+		    VID padding = -u[p] & mask;
+		    nwpad += padding;
+		    part.inuse_as_array()[p] = u[p];
+		    u[p] += padding;
+		}
+		assert( u[p] > 0 );
+		part.as_array()[p] = u[p];
+		assert( (u[p] % pmul == 0) || p == P-1 );
+		if( p > 0 )
+		    s[p] = s[p-1] + u[p-1];
+	    }
+	    assert( s[P-1] + u[P-1] >= n && s[P-1] + u[P-1] < max_nwpad );
+	    assert( s[P-1] + u[P-1] == nwpad );
+
+	    // Aggregate values
+	    part.as_array()[P] = nwpad;
+	    part.compute_starts_inuse(); // same as s[]
+#endif
 	} else {
 	    s[0] = 0;
 	    part.as_array()[0] = u[0];
@@ -742,6 +816,7 @@ private:
 	    // Place vertices to partition in order of decreasing degree.
 	    // Balance partitions out if a partition is full. This should be
 	    // infrequent as it can happen only near the end of allocation.
+#if 0
 	    VID v = first[dmax];
 	    VID *remap_p = remap.get();
 	    for( VID i=0; i < n; ++i, v=next[v] ) {
@@ -751,6 +826,7 @@ private:
 		// is full. Partitions may have been shrunk so their vertex
 		// count is a multiple of maxVL.
 		while( s[a[v]] == part.start_of( a[v]+1 ) ) {
+		    assert( 0 && "should be avoided" );
 		    // Override allocation to have multiples of pmul / partition
 		    std::cerr << "v=" << v << " overflows partition " << a[v]
 			      << " move to next\n";
@@ -760,6 +836,20 @@ private:
 		remap_p[v] = s[a[v]]++;
 		a[v] = ~(VID)0;
 	    }
+#else
+	    VID v = first[dmax];
+	    VID *remap_p = remap.get();
+	    for( VID i=0; i < n; ++i, v=next[v] ) {
+		// assert( v != ~(VID)0 );
+		// assert( a[v] != ~(VID)0 );
+		// Move vertices over to next partition if current partition
+		// is full. Partitions may have been shrunk so their vertex
+		// count is a multiple of maxVL.
+		// assert( s[a[v]] != part.start_of( a[v]+1 ) );
+		remap_p[v] = s[a[v]]++;
+		a[v] = ~(VID)0;
+	    }
+#endif
 
 	    VID vpad = n;
 	    for( VID p=0; p < P; ++p ) {
@@ -767,6 +857,11 @@ private:
 		    remap_p[vpad++] = s[p]++;
 	    }
 	    assert( vpad == part.get_vertex_range() );
+
+	    for( VID p=0; p < P; ++p ) {
+		assert( s[p] == part.start_of( p+1 )
+			&& "Need to fill all partitions" );
+	    }
 
 	    // Correctness test
 	    /*
