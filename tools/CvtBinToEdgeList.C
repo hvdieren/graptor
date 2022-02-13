9b000f6bd68e59fc5c56cb46ae2c93f40511dba4
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2022-02-13 20:58:38 +0000

    optimisations

--- tools/CvtBinToEdgeList.C
+++ tools/CvtBinToEdgeList.C
@@ -8,44 +8,58 @@
 
 int main( int argc, char *argv[] ) {
     commandLine P( argc, argv, " help" );
-    char* iFile = P.getArgument(0);
     bool symmetric = P.getOptionValue("-s");
     bool binary = P.getOptionValue("-b");             //Galois binary format
 
+    const char * ifile = P.getOptionValue( "-i" );
+    const char * wfile = P.getOptionValue("-weights"); // file with weights
     const char * ofile = P.getOptionValue( "-o" );
 
-/*
-    std::cerr << "Reading Ligra graph " << iFile << "\n";
-    wholeGraph<asymmetricVertex> WG =
-	readGraph<asymmetricVertex>((char *)iFile,false,binary);
-    std::cerr << "Convert to GraphCSx\n";
-    GraphCSx G( WG, -1 );
-*/
+    std::cerr << "Reading graph " << ifile << "...\n";
 
-    GraphCSx G( iFile, -1 );
+    GraphCSx G( ifile, -1, symmetric, wfile );
 
-    std::cerr << "Read graph.\n";
+    std::cerr << "Creating output file " << ofile << "...\n";
 
-    {
-	std::cerr << "Create COO.\n";
-	GraphCOO GCSR( G, -1 );
+    std::string wel = std::string( ofile );
+    ofstream file( wel, ios::out | ios::trunc );
 
-	std::cerr << "Rejig data.\n";
-	uint64_t n = GCSR.numVertices();
-	uint64_t m = GCSR.numEdges();
-
-	std::cerr << "Write to file.\n";
-	std::string csrfile = std::string( ofile );
-	ofstream file( csrfile, ios::out | ios::trunc );
-
-	for( EID i=0; i < m; ++i )
-	    file << GCSR.getSrc()[i] << ' ' << GCSR.getDst()[i] << '\n';
-
-	GCSR.del();
-
-	file.close();
-	std::cerr << "Wrote edge list.\n";
+    if( !file ) {
+	std::cerr << "Could not create '" << ofile << "': "
+		  << strerror( errno ) << "\n";
+	return 1;
     }
+
+    VID n = G.numVertices();
+    EID m = G.numEdges();
+    EID * index = G.getIndex();
+    VID * edges = G.getEdges();
+    float * weights = G.getWeights() ? G.getWeights()->get() : nullptr;
+
+    constexpr size_t THRESHOLD = size_t(1) << 32; // 4 GiB
+    std::stringstream buffer;
+
+    for( VID u=0; u < n; ++u ) {
+	EID es = index[u];
+	EID ee = index[u+1];
+	for( EID e=es; e < ee; ++e ) {
+	    VID v = edges[e];
+
+	    if( (size_t)buffer.tellp() >= THRESHOLD ) {
+		file << buffer.rdbuf();
+		std::stringstream().swap(buffer);
+	    }
+
+	    buffer << u << ' ' << v;
+	    if( weights )
+		buffer << ' ' << weights[e];
+	    buffer << '\n';
+	}
+    }
+
+    file << buffer.rdbuf();
+    file.close();
+    std::cerr << "Wrote edge list.\n";
 
     return 0;
 }
