1560d01f5c6f784e0ae3ff9b4e6f7ab616eb3a6a
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2022-02-15 23:20:41 +0000

    redo based on DSSSP (remove delta and buckets)

--- bench/BFv.C
+++ bench/BFv.C
@@ -3,8 +3,10 @@
 #include "graptor/graptor.h"
 #include "graptor/api.h"
 #include "path.h"
+#include "buckets.h"
 
 using expr::_0;
+using expr::_1;
 
 // By default set options for highest performance
 #ifndef DEFERRED_UPDATE
@@ -33,11 +35,49 @@ using expr::_0;
 #define VARIANT 0
 #endif
 
+#ifndef SP_THRESHOLD
+#define SP_THRESHOLD -1
+#endif
+
+struct BF_customfp_config {
+    static constexpr size_t bit_size = 16;
+};
+
+using cfp_cfg = variable_customfp_config<BF_customfp_config>;
+
+using SlotID = int32_t;
+using USlotID = uint32_t;
+
 #if VARIANT == 0
 using FloatTy = float;
 using SFloatTy = float;
 using EncCur = array_encoding<FloatTy>;
 using EncNew = array_encoding<FloatTy>;
+using EncEdge = array_encoding<FloatTy>;
+#elif VARIANT == 1
+using FloatTy = float;
+// using SFloatTy = scustomfp<false,7,9,true,6>;
+using SFloatTy = scustomfp<false,5,11,true,15>;
+using EncCur = array_encoding_wide<SFloatTy>;
+using EncNew = EncCur;
+using EncEdge = array_encoding<FloatTy>;
+#elif VARIANT == 2
+using FloatTy = float;
+using SFloatTy = vcustomfp<cfp_cfg>;
+using EncCur = array_encoding<SFloatTy>;
+using EncNew = EncCur;
+using EncEdge = array_encoding<FloatTy>;
+#elif VARIANT == 3
+using FloatTy = float;
+using SFloatTy = scustomfp<false,5,11,true,21>;
+using EncCur = array_encoding_wide<SFloatTy>;
+using EncNew = EncCur;
+using EncEdge = array_encoding<FloatTy>;
+#elif VARIANT == 4
+using FloatTy = float;
+using SFloatTy = scustomfp<false,5,11,true,30>;
+using EncCur = array_encoding_wide<SFloatTy>;
+using EncNew = EncCur;
 using EncEdge = array_encoding<FloatTy>;
 #else
 #error "Bad value for VARIANT"
@@ -52,19 +92,22 @@ using EdgeTy = typename EncEdge::stored_type;
 enum variable_name {
     var_cur = 0,
     var_new = 1,
-    var_weight = 2,
     var_min = 3,
     var_max = 4,
     var_remap = 5,
     var_cur2 = 6,
-    var_new2 = 7
+    var_new2 = 7,
+    var_d = 8,
+    var_neg = 10,
+    var_zero = 11
 };
 
 template <class GraphType>
-class BFv {
+class BF {
 public:
-    BFv( GraphType & _GA, commandLine & P )
-	: GA( _GA ), info_buf( 60 ),
+    BF( GraphType & _GA, commandLine & P )
+	: GA( _GA ),
+	  info_buf( 60 ),
 #if !LEVEL_ASYNC || DEFERRED_UPDATE
 	  cur_dist( GA.get_partitioner(), "current distance" ),
 #endif
@@ -73,45 +116,12 @@ public:
 	itimes = P.getOption( "-itimes" );
 	debug = P.getOption( "-debug" );
 	calculate_active = P.getOption( "-cactive" );
-	est_diam = P.getOptionLongValue( "-bf:diam", 20 );
+	est_diam = P.getOptionLongValue( "-sssp:diam", 20 );
 	start = GA.remapID( P.getOptionLongValue( "-start", 0 ) );
 
-	// Initialise (random) edge weights
-	VID n = GA.numVertices();
-	auto remap = GA.get_remapper();
-	if constexpr ( remap.is_idempotent() ) {
-	    api::edgemap(
-		GA,
-		api::relax( [&]( auto s, auto d, auto e ) {
-		    auto cR = expr::constant_val2<VID>( d, 127 );
-		    auto cO = expr::constant_val2<FloatTy>( d, 64 );
-		    auto cW = expr::constant_val2<FloatTy>( d, 64 * 16 );
-		    auto val = ( s + d ) & cR;
-		    return expr::make_seq(
-			edge_weight[e] =
-			expr::constant_val2<FloatTy>( d, 0.125 )
-			+ ( expr::cast<FloatTy>( val ) - cO ) / cW,
-			expr::true_val( d ) );
-		} )
-		).materialize();
-	} else {
-	    expr::array_ro<VID,VID,var_remap> a_remap( remap.getOrigIDPtr() );
-	    api::edgemap(
-		GA,
-		api::relax( [&]( auto s, auto d, auto e ) {
-		    auto ss = a_remap[s];
-		    auto dd = a_remap[d];
-		    auto cR = expr::constant_val2<VID>( d, 127 );
-		    auto cO = expr::constant_val2<FloatTy>( d, 64 );
-		    auto cW = expr::constant_val2<FloatTy>( d, 64 * 16 );
-		    auto val = ( ss + dd ) & cR;
-		    return expr::make_seq(
-			edge_weight[e] =
-			expr::constant_val2<FloatTy>( d, 0.125 )
-			+ ( expr::cast<FloatTy>( val ) - cO ) / cW,
-			expr::true_val( d ) );
-		} )
-		).materialize();
+	if( GA.getWeights() == nullptr ) {
+	    std::cerr << "BF requires edge weights. Terminating.\n";
+	    exit( 1 );
 	}
 
 	static bool once = false;
@@ -122,11 +132,22 @@ public:
 	    std::cerr << "LEVEL_ASYNC=" << LEVEL_ASYNC << "\n";
 	    std::cerr << "CONVERGENCE=" << CONVERGENCE << "\n";
 	    std::cerr << "MEMO=" << MEMO << "\n";
+	    std::cerr << "VARIANT=" << VARIANT << "\n";
+	    std::cerr << "SP_THRESHOLD=" << SP_THRESHOLD << "\n";
 	    std::cerr << "sizeof(FloatTy)=" << sizeof(FloatTy) << "\n";
 	    std::cerr << "sizeof(SFloatTy)=" << sizeof(SFloatTy) << "\n";
+#if VARIANT != 2
+	    using ft = fp_traits<SFloatTy>;
+	    std::cerr << "FP: sign bit=" << ft::sign_bit << "\n";
+	    std::cerr << "FP: exponent bits=" << ft::exponent_bits << "\n";
+	    std::cerr << "FP: mantissa bits=" << ft::mantissa_bits << "\n";
+	    std::cerr << "FP: exponent truncated=" << ft::exponent_truncated << "\n";
+	    std::cerr << "FP: can hold zero=" << ft::maybe_zero << "\n";
+	    std::cerr << "FP: exponent bias=" << ft::exponent_bias << "\n";
+#endif
 	}
     }
-    ~BFv() {
+    ~BF() {
 #if !LEVEL_ASYNC || DEFERRED_UPDATE
 	cur_dist.del();
 #endif
@@ -139,34 +160,48 @@ public:
 	float density;
 	VID nactv;
 	EID nacte;
+	float meps;
 
 	void dump( int i ) {
 	    std::cerr << "Iteration " << i << ": " << delay
 		      << " density: " << density
 		      << " (E:" << nacte
 		      << ", V:" << nactv
-		      << ")\n";
+		      << ") M-edges/s: " << meps
+		      << "\n";
 	}
     };
 
-    struct stat { };
+    struct stat {
+	FloatTy shortest, longest;
+    };
 
     void run() {
 	const partitioner &part = GA.get_partitioner();
 	VID n = GA.numVertices();
 	EID m = GA.numEdges();
 
+	timer tm_iter;
+	tm_iter.start();
+
+	iter = 0;
+	active = 1;
+
 #if !LEVEL_ASYNC || DEFERRED_UPDATE
 	assert( cur_dist.get_ptr() );
 #endif
 
-#if VARIANT != 0
+#if VARIANT == 2
 	// Analyse exponent range of edge weights, and infer floating-point
 	// format.
 	FloatTy f_min = std::numeric_limits<FloatTy>::max();
-	FloatTy f_max = -std::numeric_limits<FloatTy>::max();
+	FloatTy f_max = std::numeric_limits<FloatTy>::lowest();
+	bool f_neg = false;
+	bool f_zero = false;
 	expr::array_ro<FloatTy,VID,var_min> a_min( &f_min );
 	expr::array_ro<FloatTy,VID,var_max> a_max( &f_max );
+	expr::array_ro<bool,VID,var_neg> a_neg( &f_neg );
+	expr::array_ro<bool,VID,var_zero> a_zero( &f_zero );
 #if 0
 	// TODO: How to filter out padding edges? Need reference to graph.
 	//       It would be faster to perform a flat edge map, however, to do
@@ -177,8 +212,13 @@ public:
 	    .flat_edge_map( [&]( auto e ) {
 		auto z =
 		    expr::value<simd::ty<VID,decltype(e)::VL>,expr::vk_zero>();
-		return make_seq( a_min[z].min( edge_weight[e] ),
-				 a_max[z].max( edge_weight[e] ) );
+		return make_seq(
+		    a_neg[z] |= edge_weight[e] < _0,
+		    a_zero[z] |= edge_weight[e] == _0,
+		    a_min[z].min(
+			expr::add_mask( expr::abs( edge_weight[e] ),
+					edge_weight[e] != _0 ) ),
+		    a_max[z].max( edge_weight[e] ) );
 	    } )
 	    .materialize();
 #else
@@ -187,8 +227,13 @@ public:
 	    api::relax( [&]( auto s, auto d, auto e ) {
 		auto z =
 		    expr::value<simd::ty<VID,decltype(e)::VL>,expr::vk_zero>();
-		return make_seq( a_min[z].min( edge_weight[e] ),
-				 a_max[z].max( edge_weight[e] ) );
+		return make_seq(
+		    a_neg[z] |= expr::cast<bool>( expr::make_unop_switch_to_vector( edge_weight[e] < _0 ) ),
+		    a_zero[z] |= expr::cast<bool>( expr::make_unop_switch_to_vector( edge_weight[e] == _0 ) ),
+		    a_min[z].min(
+			expr::add_predicate( expr::abs( edge_weight[e] ),
+					     edge_weight[e] != _0 ) ),
+		    a_max[z].max( edge_weight[e] ) );
 	    } ) )
 	    .materialize();
 #endif
@@ -196,7 +241,42 @@ public:
 	std::cout << "Estimated diameter: " << est_diam << "\n";
 	std::cout << "Smallest edge_weight: " << f_min << "\n";
 	std::cout << "Largest edge_weight: " << f_max << "\n";
+	std::cout << "Any negative edge_weight: " << f_neg << "\n";
+	std::cout << "Any zero edge_weight: " << f_zero << "\n";
 	std::cout << "Longest path: " << f_max*(FloatTy)est_diam << "\n";
+
+	// Determine configuration of custom floating-point format, assuming
+	// maximum path length is est_diam hubs at most.
+	// The shortest path length is 0 (from vertex to itself), however,
+	// this is an exceptional case and we may detect and handle it, or
+	// assume that the smallest path length can be 0.
+	// Here, we require that 0 and infinity are correctly represented.
+#if VARIANT != 0 && VARIANT != 2
+	std::cout << "Smallest float: " << SFloatTy::min().get()
+		  << " = " << (float)SFloatTy::min() << "\n";
+	std::cout << "Largest float: " << SFloatTy::max().get()
+		  << " = " << (float)SFloatTy::max() << "\n";
+	assert( f_min >= (float)SFloatTy::min()
+		&& f_max <= (float)SFloatTy::max()
+		&& "edge weights out of range for data type" );
+#elif VARIANT == 2
+	// Need to add up to this many, worst case
+	f_max *= (FloatTy)est_diam;
+
+	// Ensure we can at least encode the value 1.0
+	f_max = std::max( f_max, 1.0f );
+
+	cfp_cfg::set_param( f_min, f_max, f_neg, /*f_zero*/true, false );
+	auto vz = typename EncCur::stored_type(0.0f);
+	std::cout << "Configuration (0.0f value): "
+		  << vz << " -> " << (float)vz << "\n";
+	auto vo = typename EncCur::stored_type(1.0f);
+	std::cout << "Configuration (1.0f value): "
+		  << vo << " -> " << (float)vo << "\n";
+	auto vh = typename EncCur::stored_type(0.5f);
+	std::cout << "Configuration (0.5f value): "
+		  << vh << " -> " << (float)vh << "\n";
+#endif
 
 	static_assert( std::is_same_v<EdgeTy,FloatTy>,
 		       "requires re-coding of edges" );
@@ -208,7 +288,7 @@ public:
 #if VARIANT == 0
 		auto inf = expr::constant_val2<SFloatTy>(
 		    v, std::numeric_limits<SFloatTy>::infinity() );
-#elif VARIANT == 1 || VARIANT == 3
+#elif VARIANT != 2
 		auto inf = expr::constant_val2<SFloatTy>(
 		    v, SFloatTy::max() );
 #else
@@ -220,48 +300,30 @@ public:
 		     cur_dist[v] = inf,
 #endif
 		     new_dist[v] = inf );
-	    }
-		)
+	    } )
 	    .materialize();
 #if !LEVEL_ASYNC || DEFERRED_UPDATE
 	cur_dist.get_ptr()[start] = 0.0f;
 #endif
 	new_dist.get_ptr()[start] = 0.0f;
 
-	// Create initial frontier
 	frontier F = frontier::create( n, start, GA.getOutDegree(start) );
 
-	iter = 0;
-	active = 1;
+	if( itimes ) {
+	    info_buf.resize( iter+1 );
+	    info_buf[iter].density = 0;
+	    info_buf[iter].nactv = 0;
+	    info_buf[iter].nacte = 0;
+	    info_buf[iter].delay = tm_iter.next();
+	    info_buf[iter].meps = 
+		float(info_buf[iter].nacte) / info_buf[iter].delay / 1e6f;
+	    if( debug )
+		info_buf[iter].dump( iter );
+	}
+
+	++iter;
 
 	while( !F.isEmpty() ) {  // iterate until all vertices visited
-	    timer tm_iter;
-	    tm_iter.start();
-
-#if VARIANT == 3
-#if 1
-	    f_max = -std::numeric_limits<FloatTy>::max();
-	    make_lazy_executor( part )
-		.vertex_scan( [&]( auto v ) {
-		    auto z =
-			expr::value<simd::ty<VID,decltype(v)::VL>,expr::vk_zero>();
-		    auto inf = expr::constant_val2<FloatTy>(
-			v, (FloatTy)SFloatTy::max() );
-		    return expr::set_mask( cur_dist[v] != inf,
-					   a_max[z].max( cur_dist[v] ) );
-		} )
-		.materialize();
-	    // std::cout << "Shortest path: " << f_min << "\n";
-	    // std::cout << "Longest path: " << f_max << "\n";
-	    if( f_max >= f_min * 256 )
-		break;
-#endif
-#endif
-
-	    // If we have a sparse frontier that is unbacked, we cannot
-	    // do sparse processing. Hence, perform another dense edgemap
-	    // but one which produces a backed frontier.
-	    
 	    // Traverse edges, remove duplicate live destinations.
 	    frontier output;
 #if UNCOND_EXEC
@@ -270,15 +332,23 @@ public:
 	    auto filter_strength = api::strong;
 #endif
 
+	    // TODO: config to not calculate nactv, nacte?
 	    api::edgemap(
 		GA, 
+#if SP_THRESHOLD >= 0
+		api::config( api::frac_threshold( SP_THRESHOLD ) ),
+#endif
 #if DEFERRED_UPDATE
+		// Allow for (sparse) push to use reduction update to inform
+		// frontier rather than using the method.
 		api::record( output,
+			     api::reduction_or_method,
 			     [&] ( auto d ) {
-				 return cur_dist[d] != new_dist[d]; },
-			     filter_strength ),
+			     return cur_dist[d] != new_dist[d]; },
+			     api::strong ),
 #else
-		api::record( output, api::reduction, filter_strength ),
+		// Do not allow unbacked frontiers
+		api::record( output, api::reduction, api::strong ),
 #endif
 		api::filter( filter_strength, api::src, F ),
 		api::relax( [&]( auto s, auto d, auto e ) {
@@ -289,85 +359,9 @@ public:
 #endif
 		} )
 		).materialize();
-
-#if 0
-	    map_vertexL( part,
-			 [&]( VID i ) {
-			     if( cur_dist[i] > new_dist[i]
-				 && cur_dist[i] != (float)SFloatTy::max() ) {
-				 std::cout << "       decreasing path: "
-					   << " i=" << i
-					   << " cur=" << cur_dist[i]
-					   << ", " << cur_dist.get_ptr()[i]
-					   << " new=" << new_dist[i]
-					   << ", " << new_dist.get_ptr()[i]
-					   << "\n";
-			     } } );
-#endif
-
-#if 0
-	    std::cout << "F     : " << F << "\n";
-	    std::cout << "output: " << output << "\n";
+			   
 #if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    print( std::cout, GA.get_remapper(), part, cur_dist );
-#endif
-	    print( std::cout, GA.get_remapper(), part, new_dist );
-
-	    std::cout << "F[29] " << F.is_set( 29 ) << "\n";
-	    std::cout << "output[29] " << output.is_set( 29 ) << "\n";
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    std::cout << "cur_dist[29] " << cur_dist[29] << "\n";
-#endif
-	    std::cout << "new_dist[29] " << new_dist[29] << "\n";
-
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    if( output.getType() == frontier_type::ft_logical4 ) {
-		logical<4> * f
-		    = output.template getDense<frontier_type::ft_logical4>();
-		VID k = 0;
-		for( VID v=0; v < n; ++v ) {
-		    if( cur_dist[v] != new_dist[v] ) {
-/*
-			std::cout << "diff: " << cur_dist[v] << ' '
-				  << new_dist[v] << " k=" << k
-				  << " v=" << v << "\n";
-*/
-			assert( f[v] != 0 );
-			++k;
-		    } else
-			assert( f[v] == 0 );
-		}
-		assert( k == output.nActiveVertices() );
-	    }
-#endif
-#endif
-
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    // A sparse frontier that cannot be automatically converted due to
-	    // being unbacked.
-	    if( F.getType() == frontier_type::ft_unbacked
-		&& api::default_threshold().is_sparse( output, m ) ) {
-		frontier ftrue = frontier::all_true( n, m );
-		frontier output2;
-		make_lazy_executor( part )
-		    .vertex_filter( GA, ftrue, output2,
-				    [&]( auto v ) {
-					return cur_dist[v] != new_dist[v];
-				    } )
-		    .materialize();
-
-		assert( output.nActiveVertices() == output2.nActiveVertices()
-			&& "check number of vertices is the same" );
-		assert( output.nActiveEdges() == output2.nActiveEdges()
-			&& "check number of edges is the same" );
-
-		output.del();
-		output = output2;
-	    }
-#endif
-
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    if( output.getType() == frontier_type::ft_unbacked /*|| 1*/ ) {
+	    if( output.getType() == frontier_type::ft_unbacked ) {
 		make_lazy_executor( part )
 		    .vertex_map( [&]( auto v ) {
 			return cur_dist[v] = new_dist[v];
@@ -380,8 +374,6 @@ public:
 		    } )
 		    .materialize();
 	    }
-	    // maintain_copies( part, output, cur_dist.get_ptr(),
-	    // new_dist.get_ptr() );
 #endif
 
 	    active += output.nActiveVertices();
@@ -392,112 +384,18 @@ public:
 		info_buf[iter].nactv = F.nActiveVertices();
 		info_buf[iter].nacte = F.nActiveEdges();
 		info_buf[iter].delay = tm_iter.next();
+		info_buf[iter].meps = 
+		    float(info_buf[iter].nacte) / info_buf[iter].delay / 1e6f;
 		if( debug )
 		    info_buf[iter].dump( iter );
 	    }
 
-	    // Cleanup old frontier
+	    // Cleanup frontier
 	    F.del();
-	    F = output;
+	    F  = output;
 
 	    ++iter;
 	}
-
-#if VARIANT == 3
-	if( !F.isEmpty() ) {  // need a second phase
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-	    api::vertexprop<FloatTy,VID,var_cur2> cur_dist2(
-		GA.get_partitioner(), "current distance (2)" );
-#endif
-	    api::vertexprop<FloatTy,VID,var_new2> new_dist2(
-		GA.get_partitioner(), "new distance (2)" );
-
-	    make_lazy_executor( part )
-		.vertex_map( [&]( auto v ) {
-		    return expr::make_seq(
-			cur_dist2[v] = cur_dist[v],
-			new_dist2[v] = new_dist[v] );
-		} )
-		.materialize();
-
-	    while( !F.isEmpty() ) {  // iterate until all vertices visited
-		timer tm_iter;
-		tm_iter.start();
-
-		// Traverse edges, remove duplicate live destinations.
-		frontier output;
-#if UNCOND_EXEC
-		auto filter_strength = api::weak;
-#else
-		auto filter_strength = api::strong;
-#endif
-		api::edgemap(
-		    GA, 
-#if DEFERRED_UPDATE
-		    api::record( output,
-				 [&] ( auto d ) {
-				     return cur_dist2[d] != new_dist2[d]; },
-				 filter_strength ),
-#else
-		    api::record( output, api::reduction, filter_strength ),
-#endif
-		    api::filter( filter_strength, api::src, F ),
-		    api::relax( [&]( auto s, auto d, auto e ) {
-#if LEVEL_ASYNC
-			return new_dist2[d].min( new_dist2[s] + edge_weight[e] );
-#else
-			return new_dist2[d].min( cur_dist2[s] + edge_weight[e] );
-#endif
-				} )
-		    ).materialize();
-
-#if !LEVEL_ASYNC || DEFERRED_UPDATE
-		if( output.getType() == frontier_type::ft_unbacked || 1 ) {
-		    make_lazy_executor( part )
-			.vertex_map( [&]( auto v ) {
-			    return cur_dist2[v] = new_dist2[v];
-			} )
-			.materialize();
-		} else {
-		    make_lazy_executor( part )
-			.vertex_map( output, [&]( auto v ) {
-			    return cur_dist2[v] = new_dist2[v];
-			} )
-			.materialize();
-		}
-#endif
-
-		active += output.nActiveVertices();
-
-		if( itimes ) {
-		    info_buf.resize( iter+1 );
-		    info_buf[iter].density = F.density( GA.numEdges() );
-		    info_buf[iter].nactv = F.nActiveVertices();
-		    info_buf[iter].nacte = F.nActiveEdges();
-		    info_buf[iter].delay = tm_iter.next();
-		    if( debug )
-			info_buf[iter].dump( iter );
-		}
-
-		// Cleanup old frontier
-		F.del();
-		F = output;
-
-		++iter;
-	    }
-
-	    make_lazy_executor( part )
-		.vertex_map( [&]( auto v ) {
-		    return expr::make_seq(
-			cur_dist[v] = cur_dist2[v],
-			new_dist[v] = new_dist2[v] );
-		} )
-		.materialize();
-
-	    cur_dist2.del();
-	    new_dist2.del();
-	}
-#endif // VARIANT == 3
 
 	F.del();
     }
@@ -509,14 +407,36 @@ public:
 	}
     }
 
-    static void report( const std::vector<stat> & stat_buf ) { }
+    static void report( const std::vector<stat> & stat_buf ) {
+	bool fail = false;
+	for( int i=1; i < stat_buf.size(); ++i ) {
+	    if( stat_buf[i].shortest != stat_buf[0].shortest ) {
+		std::cerr << "ERROR: round " << i << " has shortest path="
+			  << stat_buf[i].shortest
+			  << " while round 0 has shortest path="
+			  << stat_buf[0].shortest << "\n";
+		fail = true;
+	    }
+	    if( stat_buf[i].longest != stat_buf[0].longest ) {
+		std::cerr << "ERROR: round " << i << " has longest path="
+			  << stat_buf[i].longest
+			  << " while round 0 has longest path="
+			  << stat_buf[0].longest << "\n";
+		fail = true;
+	    }
+	}
+	if( fail )
+	    abort();
+    }
 
     void validate( stat & stat_buf ) {
 	VID n = GA.numVertices();
-	const partitioner &part = GA.get_partitioner();
 
 	FloatTy shortest, longest;
 	std::tie( shortest, longest ) = find_min_max( GA, new_dist );
+
+	stat_buf.shortest = shortest;
+	stat_buf.longest = longest;
 
 	std::cout << "Shortest path from " << start
 		  << " (original: " << GA.originalID( start ) << ") : "
@@ -544,11 +464,11 @@ private:
     api::vertexprop<FloatTy,VID,var_cur,EncCur> cur_dist;
 #endif
     api::vertexprop<FloatTy,VID,var_new,EncNew> new_dist;
-    api::edgeprop<FloatTy,EID,var_weight,EncEdge> edge_weight;
+    api::edgeprop<FloatTy,EID,expr::vk_eweight,EncEdge> edge_weight;
     std::vector<info> info_buf;
 };
 
 template <class GraphType>
-using Benchmark = BFv<GraphType>;
+using Benchmark = BF<GraphType>;
 
 #include "driver.C"
