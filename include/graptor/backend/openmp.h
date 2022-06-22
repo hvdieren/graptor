589aeacc3003750bed6a4950791fa3f33e37411d
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2022-06-22 20:57:22 +0100

    exploring openmp setup

--- include/graptor/backend/openmp.h
+++ include/graptor/backend/openmp.h
@@ -28,17 +28,46 @@ inline uint32_t graptor_num_threads() {
     return omp_get_max_threads();
 }
 
+template<typename T, typename Fn>
+void parallel_loop( const T it_start, const T it_end, Fn fn ) {
+#pragma omp parallel for
+    for( T i=it_start; i != it_end; ++i ) {
+	fn( i );
+    }
+}
+
+template<typename T, typename Fn>
+void parallel_loop( const T it_start, const T it_end, const int grain, Fn fn ) {
+#pragma omp parallel for schedule(static,grain)
+    for( T i=it_start; i != it_end; ++i ) {
+	fn( i );
+    }
+}
+
 #if NUMA
 template<typename Fn>
 void map_partitionL( const partitioner & part, Fn fn ) {
+#if 0
 #pragma omp parallel for num_threads(num_numa_node) proc_bind(spread)
     for( unsigned n=0; n < num_numa_node; ++n ) {
 	auto lo = part.numa_start_of(n);
 	auto hi = part.numa_end_of(n);
+	std::cerr << "nested parallel case...\n";
+// #pragma omp parallel for num_threads(??) schedule(static,1) proc_bind(close)
 #pragma omp parallel for schedule(static,1) proc_bind(close)
 	for( auto p=lo; p < hi; ++p )
 	    fn( p );
     }
+#else
+    // Shoud result in desired mapping if assuming static scheduling and
+    // #partitions multiple of threads, however, no option for dynamic
+    // scheduling to achieve automatic load balancing
+    auto np = part.get_num_partitions();
+#pragma omp parallel for
+    for( unsigned p=0; p < np; ++p ) {
+	fn( p );
+    }
+#endif
 }
 #else // not NUMA
 template<typename Fn>
