fe49fbdc5578e20696d9edd75e8119630d626a55
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2023-02-14 21:00:06 +0000

    empty mask pack utility function

--- include/graptor/dsl/aval.h
+++ include/graptor/dsl/aval.h
@@ -493,6 +493,10 @@ auto create_mask_pack( simd::detail::mask_impl<MTr> m ) {
     return mask_pack<MTr>( m );
 }
 
+auto create_mask_pack() {
+    return mask_pack<>();
+}
+
 template<typename T>
 struct is_mask_pack : public std::false_type { };
 
