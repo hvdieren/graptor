12a9f95ef1749c54b6e7eef35da445061201bd50
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2023-04-03 15:35:09 +0000

    fusion only in multi-threaded execution

--- include/graptor/dsl/edgemap.h
+++ include/graptor/dsl/edgemap.h
@@ -1428,6 +1428,7 @@ static __attribute__((noinline)) frontier csr_sparse_with_f(
 	// if( /* !expr::is_readonly_fusion_op<Operator>::value
 	// || */ ( cfg.is_parallel() && /* m >= 1024 */ mm_parts >= mm_min ) || true ) {
 	if( cfg.is_parallel()
+	    && graptor_num_threads() > 1
 	    && cfg.do_fusion( m, mm, GA.numVertices() ) ) {
 	    delete[] degree;
 	    return csr_sparse_with_f_fusion_stealing(
