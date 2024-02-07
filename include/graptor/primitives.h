98cd162545e5acf59a071f032a53d6fed7775a7a
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-02-07 22:01:24 +0000

    add clear primitive, add vmap_timing

--- include/graptor/primitives.h
+++ include/graptor/primitives.h
@@ -8,10 +8,16 @@
 #include "graptor/backend/backend.h"
 #include "graptor/partitioner.h"
 #include "graptor/itraits.h"
+#include "graptor/vmap_timing.h"
 
 template<typename T>
 void __attribute__((noinline))
 fill_by_partition( const partitioner & part, T * ptr, T && val ) {
+#if VMAP_TIMING
+    timer tm;
+    tm.start();
+#endif
+
     if constexpr ( is_logical_v<T> ) {
 	// Logical types do not trigger call of memset; make it easier
 	// for the compiler by digging up the underlying type.
@@ -36,16 +42,54 @@ fill_by_partition( const partitioner & part, T * ptr, T && val ) {
 		std::fill<T *,T>( &ptr[ps], &ptr[pe], val );
 	    } );
     }
+
+#if VMAP_TIMING
+    struct tag { };
+    vmap_record_time<tag>( tm.next() );
+#endif
 }
 
 template<typename T>
 void __attribute__((noinline))
+clear_by_partition( const partitioner & part, T * ptr ) {
+#if VMAP_TIMING
+    timer tm;
+    tm.start();
+#endif
+
+    // Specialise to memset as this is the fastest option, better than std::fill
+    map_partitionL( part, [=,&part]( unsigned p ) {
+	VID ps = part.start_of( p );
+	VID pe = part.end_of( p );
+	char * cps = reinterpret_cast<char *>( &ptr[ps] );
+	char * cpe = reinterpret_cast<char *>( &ptr[pe] );
+	memset( cps, (char)0, cpe-cps );
+    } );
+
+#if VMAP_TIMING
+    struct tag { };
+    vmap_record_time<tag>( tm.next() );
+#endif
+}
+
+
+template<typename T>
+void __attribute__((noinline))
 copy_by_partition( const partitioner & part, T * to, const T * from ) {
+#if VMAP_TIMING
+    timer tm;
+    tm.start();
+#endif
     map_partitionL( part, [=,&part]( unsigned p ) {
 	    VID ps = part.start_of( p );
 	    VID pe = part.end_of( p );
 	    std::copy( &from[ps], &from[pe], &to[ps] );
 	} );
+
+#if VMAP_TIMING
+    struct tag { };
+    vmap_record_time<tag>( tm.next() );
+#endif
 }
 
 #endif // GRAPTOR_PRIMITIVES_H
