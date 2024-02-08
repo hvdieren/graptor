0a54124ae3f110ef98691c1021ccb38641384403
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-02-08 10:52:19 +0000

    conditional parallelism

--- include/graptor/frontier/impl.h
+++ include/graptor/frontier/impl.h
@@ -704,16 +704,23 @@ void frontier::toBit( const partitioner & part ) {
 	unsigned char *bp = b.get();
 	clear_by_partition( cpart, bp );
 
-	using Enc = array_encoding_bit<1>;
-	using data_type = simd::ty<bitfield<1>,1>;
-	parallel_loop( (VID)0, nactv, [&]( VID i ) {
-	    VID v = s[i];
-	    auto l = Enc::extract<data_type>( Enc::ldcas<data_type>( bp, v ) );
-	    auto ll = bitfield<1>( 1 );
-	    while( !Enc::cas<data_type>( bp, v, l, ll ) ) {
-		l = Enc::extract<data_type>( Enc::ldcas<data_type>( bp, v ) );
+	if( nactv > 4096 ) {
+	    using Enc = array_encoding_bit<1>;
+	    using data_type = simd::ty<bitfield<1>,1>;
+	    parallel_loop( (VID)0, nactv, [&]( VID i ) {
+		VID v = s[i];
+		auto l = Enc::extract<data_type>( Enc::ldcas<data_type>( bp, v ) );
+		auto ll = bitfield<1>( 1 );
+		while( !Enc::cas<data_type>( bp, v, l, ll ) ) {
+		    l = Enc::extract<data_type>( Enc::ldcas<data_type>( bp, v ) );
+		}
+	    } );
+	} else {
+	    for( VID i=0; i < nactv; i++ ) {
+		VID idx = s[i] / 8;
+		bp[idx] |= ((unsigned char)1) << ( s[i] % 8 );
 	    }
-	} );
+	}
 	
 	delete[] s;
 	ftype = frontier_type::ft_bit;
