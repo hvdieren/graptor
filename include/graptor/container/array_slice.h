f6bd5280fa8dc42276cf9592f0d63b90ed886c0e
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-05-02 14:14:57 +0000

    making read-only, add trim method

--- include/graptor/container/array_slice.h
+++ include/graptor/container/array_slice.h
@@ -9,20 +9,22 @@ struct array_slice {
     using type = T;
     using index_type = I;
 
-    array_slice( type * begin_, type * end_ )
+    array_slice( const type * begin_, const type * end_ )
 	: m_begin( begin_ ), m_end( end_ ) { }
-    array_slice( type * begin_, index_type len_ )
+    array_slice( const type * begin_, index_type len_ )
 	: m_begin( begin_ ), m_end( begin_ + len_ ) { }
 
     index_type size() const { return m_end - m_begin; }
     
-    type * begin() { return m_begin; }
     const type * begin() const { return m_begin; }
-    type * end() { return m_end; }
     const type * end() const { return m_end; }
 
+    array_slice<T,I> trim_r( const type * r ) const {
+	return array_slice<T,I>( m_begin, r );
+    }
+
 private:
-    type * m_begin, * m_end;
+    const type * m_begin, * m_end;
 };
 
 } // namespace graptor
