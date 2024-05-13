2b55febb1cc6203cf5d0c2356b546890c3db7951
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-05-13 19:18:03 +0000

    add end iterator

--- include/graptor/container/conditional_iterator.h
+++ include/graptor/container/conditional_iterator.h
@@ -2,6 +2,8 @@
 #ifndef GRAPTOR_CONTAINER_CONDITIONAL_ITERATOR_H
 #define GRAPTOR_CONTAINER_CONDITIONAL_ITERATOR_H
 
+#include<iterator>
+
 namespace graptor {
 
 // Assumes std::iterator_traits<Iterator>::iterator_category is compatible
@@ -9,22 +11,23 @@ namespace graptor {
 template<typename Iterator, typename Cond>
 class conditional_iterator {
 public:
-    using type = typename Iterator::value_type;
-
     // iterator traits
     using iterator_category = std::input_iterator_tag;
-    using value_type = typename Iterator::value_type;
-    using difference_type = typename Iterator::difference_type;
-    using pointer = typename Iterator::pointer;
-    using reference = typename Iterator::reference;
+    using value_type = std::iter_value_t<Iterator>;
+    using difference_type = std::iter_difference_t<Iterator>;
+    using pointer = std::iter_value_t<Iterator>;
+    using reference = std::iter_common_reference_t<Iterator>;
 
 public:
-    explicit conditional_iterator( Iterator it, Cond cond )
-	: m_it( it ), m_cond( cond ) { }
+    explicit conditional_iterator( Iterator it, Iterator it_end, Cond cond )
+	: m_it( it ), m_it_end( it_end ), m_cond( cond ) {
+	while( m_it != m_it_end && !m_cond( *m_it ) )
+	    ++m_it;
+    }
     conditional_iterator& operator++() {
 	do {
-	    m_it++;
-	} while( !m_cond( m_it ) );
+	    ++m_it;
+	} while( m_it != m_it_end && !m_cond( *m_it ) );
 	return *this;
     }
     conditional_iterator operator++( int ) {
@@ -42,13 +45,16 @@ public:
 
 private:
     Iterator m_it;
+    Iterator m_it_end;
     Cond m_cond;
 };
 
 template<typename Iterator, typename Cond>
-auto make_conditional_iterator( Iterator && it, Cond && cond ) {
+auto make_conditional_iterator( Iterator && it, Iterator && end,
+				Cond && cond ) {
     return conditional_iterator<Iterator,Cond>(
 	std::forward<Iterator>( it ),
+	std::forward<Iterator>( end ),
 	std::forward<Cond>( cond ) );
 }
 
