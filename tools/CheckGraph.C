649404642f9d2310901aec82374ddd87c78cba04
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-08-17 11:00:33 +0000

    set upper bound on verbosely reported errors

--- tools/CheckGraph.C
+++ tools/CheckGraph.C
@@ -12,15 +12,19 @@ int main( int argc, char *argv[] ) {
 	argc, argv,
 	"\t-s\t\tinput graph is symmetric (only required for specific formats)\n"
 	"\t-i {file}\tinput file containing graph\n"
-	"\t-weights {file}\toutput file for weights of graph\n"
+	"\t-weights {file}\tinput file for weights of graph\n"
+	"\t--max {number}\tmaximum number of errors of each type to report [default: 10]\n"
 	);
 
-    const bool symmetric = P.get_bool_option("-s");
+    const bool symmetric = P.get_bool_option( "-s" );
     const char * ifile = P.get_string_option( "-i" );
-    const char * weights = P.get_string_option("-weights"); // file with weights
+    const char * weights = P.get_string_option( "-weights" );
+    const EID max_err = P.get_long_option( "--max", 100 );
 
     std::cerr << "Reading graph " << ifile << "...\n";
     GraphCSx G( ifile, -1, symmetric, weights );
+
+    EID err_sort = 0, err_dup = 0, err_range = 0;
 
     // Validate if all neighbour lists are sorted, and unique
     const EID * index = G.getIndex();
@@ -30,22 +34,37 @@ int main( int argc, char *argv[] ) {
 	EID es = index[v];
 	EID ee = index[v+1];
 	for( EID e=es+1; e < ee; ++e ) {
-	    if( edges[e] >= n )
-		std::cerr << "edge " << e << " connecting to vertex " << v
-			  << " which is out of range\n";
-	    if( edges[e-1] > edges[e] )
-		std::cerr << "violating sort order v=" << v
-			  << " e=" << (e-1) << " to " << edges[e-1]
-			  << "and e=" << e << " to " << edges[e]
-			  << "\n";
-	    else if( edges[e-1] == edges[e] )
-		std::cerr << "duplicate subsequent edges v=" << v
-			  << " e=" << (e-1) << "," << e
-			  << " to " << edges[e] << "\n";
+	    if( edges[e] >= n ) {
+		if( err_range++ < max_err )
+		    std::cerr << "edge " << e << " connecting to vertex " << v
+			      << " which is out of range\n";
+	    }
+	    if( edges[e-1] > edges[e] ) {
+		if( err_sort++ < max_err )
+		    std::cerr << "violating sort order v=" << v
+			      << " e=" << (e-1) << " to " << edges[e-1]
+			      << "and e=" << e << " to " << edges[e]
+			      << "\n";
+	    }
+	    else if( edges[e-1] == edges[e] ) {
+		if( err_dup++ < max_err )
+		    std::cerr << "duplicate subsequent edges v=" << v
+			      << " e=" << (e-1) << "," << e
+			      << " to " << edges[e] << "\n";
+	    }
 	}
     }
 
-    std::cerr << "checked " << n << " vertices\n";
+    EID m = G.numEdges();
+    if( index[n] != m )
+	std::cerr << "End of edge list marker " << index[n]
+		  << " does not match number of edges " << m << "\n";
+
+    std::cerr << "checked " << n << " vertices\n"
+	      << "sort order errors: " << err_sort
+	      << "\nduplicate edges errors: " << err_dup
+	      << "\nvertex out of range errors: " << err_range
+	      << "\n";
 
     return 0;
 }
