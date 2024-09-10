77bd73feae09570627d9497854b96b65d7edffa5
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2024-09-10 09:23:50 +0000

    lazy hashing

--- include/graptor/graph/simple/hadj.h
+++ include/graptor/graph/simple/hadj.h
@@ -25,168 +25,6 @@ namespace graptor {
 
 namespace graph {
 
-#if 0
-/*!======================================================================*
- * GraphHadj: A graph data structure that represents adjacency lists as
- * hash tables using a single pre-allocated amount of memory.
- *
- * Note: some hash functions (e.g. graptor::rand_hash) require members that
- *       impacts on the location of elements. As such, the instance of the
- *       hash function needs to be retained across constructions of the
- *       hashed adjacency list.
- *=======================================================================*/
-
-template<typename lVID, typename lEID, typename UGCSx,
-	 typename lHash = std::hash<lVID>>
-class GraphHAdj {
-public:
-    using VID = lVID;
-    using EID = lEID;
-    using Hash = lHash;
-    using UnderlyingGraphCSx = UGCSx; // GraphCSx or graptor::graph::GraphCSx<>
-    using self_type = GraphHAdj<VID,EID,UnderlyingGraphCSx,Hash>;
-    using hash_set_type = graptor::hash_set_hopscotch<VID,Hash>;
-
-    using vertex_iterator = range_iterator<VID>;
-    using edge_iterator = generic_edge_iterator<VID,EID>;
-
-    static constexpr bool has_dual_rep = false;
-
-public:
-    explicit GraphHAdj( const GraphCSx<VID,EID> & G ) :
-	GraphHAdj( G, numa_allocation_interleaved() ) { }
-    explicit GraphHAdj( const GraphCSx<VID,EID> & G,
-			numa_allocation && alloc ) :
-	m_G( G ),
-	m_index( G.numVertices()+1, alloc ),
-	m_hashes( get_hash_slots( G.getIndex(), G.numVertices() ), alloc ),
-	m_hash_fn( G.numVertices() ) {
-	VID n = G.numVertices();
-	EID h = 0;
-	EID * index = m_index.get();
-	VID * hashes = m_hashes.get();
-	EID * gindex = getIndex();
-	VID * gedges = getEdges();
-	Hash * hf = m_hash_fn.get();
-	for( VID v=0; v < n; ++v ) {
-	    VID deg = gindex[v+1] - gindex[v];
-	    VID logs = get_log_hash_slots( deg );
-	    VID s = VID(1) << logs;
-	    new ( &hf[v] ) Hash( logs ); // rand_hash function requires storage
-	    hash_set_type a( &hashes[h], 0, logs, hf( v ) );
-	    a.clear(); // initialise to invalid element
-	    a.insert( &gedges[gindex[v]], &gedges[gindex[v+1]] );
-	    index[v] = h;
-	    h += s;
-	}
-	index[n] = h;
-    }
-    explicit GraphHAdj( const ::GraphCSx & G,
-			bool parallel,
-			numa_allocation && alloc ) :
-	m_G( G ),
-	m_index( G.numVertices()+1, alloc ),
-	// m_hashes( get_hash_slots( G.getIndex(), G.numVertices() ), alloc ),
-	m_hash_fn( G.numVertices(), alloc ) {
-	VID n = G.numVertices();
-	EID h = 0;
-	EID * index = m_index.get();
-	const EID * const gindex = getIndex();
-	const VID * const gedges = getEdges();
-	parallel_loop( VID(0), n, [&]( VID v ) {
-	    VID deg = gindex[v+1] - gindex[v];
-	    index[v] = get_hash_slots( deg );
-	} );
-	index[n] = sequence::plusScan( index, index, n );
-
-	new ( &m_hashes ) mm::buffer<VID>( index[n], alloc );
-	VID * hashes = m_hashes.get();
-	Hash * hf = m_hash_fn.get();
-	parallel_loop( VID(0), n, [&]( VID v ) {
-	    VID deg = gindex[v+1] - gindex[v];
-	    VID s = index[v+1] - index[v];
-	    VID logs = rt_ilog2( s );
-	    new ( &hf[v] ) Hash( logs ); // rand_hash function requires storage
-	    hash_set_type a( &hashes[index[v]], 0, logs, hf[v] );
-	    a.clear(); // initialise to invalid element
-	    a.insert( &gedges[gindex[v]], &gedges[gindex[v+1]] );
-	} );
-    }
-    GraphHAdj( const GraphHAdj & ) = delete;
-
-    ~GraphHAdj() {
-	m_index.del();
-	m_hashes.del();
-    }
-
-    const auto & get_graph() const { return m_G; }
-
-    VID numVertices() const { return m_G.numVertices(); }
-    EID numEdges() const { return m_G.numEdges(); }
-
-    const EID * getIndex() { return m_G.getIndex(); }
-    const EID * const getIndex() const { return m_G.getIndex(); }
-    const VID * getEdges() { return m_G.getEdges(); }
-    const VID * const getEdges() const { return m_G.getEdges(); }
-    EID * getHashIndex() { return m_index.get(); }
-    const EID * getHashIndex() const { return m_index.get(); }
-    VID * getHashes() { return m_hashes.get(); }
-    const VID * getHashes() const { return m_hashes.get(); }
-
-    vertex_iterator vbegin() { return vertex_iterator( 0 ); }
-    vertex_iterator vbegin() const { return vertex_iterator( 0 ); }
-    vertex_iterator vend() { return vertex_iterator( numVertices() ); }
-    vertex_iterator vend() const { return vertex_iterator( numVertices() ); }
-
-    hash_set_type get_adjacency( VID v ) const {
-	return const_cast<self_type *>( this )->get_adjacency( v );
-    }
-    hash_set_type get_adjacency( VID v ) {
-	VID deg = getIndex()[v+1] - getIndex()[v];
-	VID h = rt_ilog2( getHashIndex()[v+1] - getHashIndex()[v] );
-	const Hash * hf = m_hash_fn.get();
-	return hash_set_type(
-	    &getHashes()[getHashIndex()[v]], deg, h, hf[v] );
-    }
-
-
-/*
-    edge_iterator ebegin() {
-	return edge_iterator( 0, 0, m_index.get(), m_edges.get() );
-    }
-    edge_iterator ebegin() const {
-	return edge_iterator( 0, 0, m_index.get(), m_edges.get() );
-    }
-    edge_iterator eend() {
-	return edge_iterator( m_n, m_m, m_index.get(), m_edges.get() );
-    }
-    edge_iterator eend() const {
-	return edge_iterator( m_n, m_m, m_index.get(), m_edges.get() );
-    }
-*/
-
-private:
-    static VID get_hash_slots( VID deg ) {
-	return VID(1) << hash_set_type::required_log_size( deg );
-    }
-    static VID get_log_hash_slots( VID deg ) {
-	return hash_set_type::required_log_size( deg );
-    }
-    static EID get_hash_slots( const EID * index, VID n ) {
-	EID h = 0;
-	for( VID v=0; v < n; ++v )
-	    h += get_hash_slots( index[v+1] - index[v] );
-	return h;
-    }
-
-private:
-    const UnderlyingGraphCSx & m_G;
-    mm::buffer<EID> m_index;
-    mm::buffer<VID> m_hashes;
-    mm::buffer<Hash> m_hash_fn;
-};
-#endif
-
 template<bool dual_rep, bool left_base, typename HashSet>
 struct hash_pa_insert_iterator {
     hash_pa_insert_iterator(
@@ -252,10 +90,11 @@ private:
 template<typename lVID, typename lEID,
 	 bool dual_rep,
 	 bool preallocate,
+	 bool lazyhash,
 	 typename lHashSet>
 class GraphHAdjPA {
 public:
-    using self_type = GraphHAdjPA<lVID,lEID,dual_rep,preallocate,lHashSet>;
+    using self_type = GraphHAdjPA<lVID,lEID,dual_rep,preallocate,lazyhash,lHashSet>;
     using VID = lVID;
     using EID = lEID;
     using hash_set_type = lHashSet;
@@ -271,10 +110,16 @@ public:
 
     static constexpr bool has_dual_rep = dual_rep;
     static constexpr bool preallocate_storage = preallocate;
+    static constexpr bool lazy_hashing = lazyhash;
+
+    static_assert( !preallocate_storage || !lazy_hashing,
+		   "Lazy hashing implemented only for non-preallocated "
+		   "storage" );
 
 public:
     explicit GraphHAdjPA( const ::GraphCSx & G,
-			  numa_allocation && alloc ) :
+			  numa_allocation && alloc,
+			  size_t lazy_threshold = 16 ) :
 	m_n( G.numVertices() ),
 	m_index( G.numVertices()+1, alloc ),
 	m_adjacency( G.numVertices(), alloc ) {
@@ -321,12 +166,16 @@ public:
 		    a.finalise();
 	    } else {
 		hash_set_type & a = m_adjacency[v];
-		new ( &a ) hash_set_type( deg ); // size hint
-		a.insert( &gedges[gindex[v]], &gedges[gindex[v+1]] );
-		if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
-		    a.finalise();
-		assert( a.size() == gindex[v+1] - gindex[v] );
-		assert( a.size() == index[v+1] - index[v] );
+		if( lazy_hashing && gindex[v+1] - gindex[v] < lazy_threshold ) {
+		    new ( &a ) hash_set_type(); // size hint empty
+		} else {
+		    new ( &a ) hash_set_type( deg ); // size hint
+		    a.insert( &gedges[gindex[v]], &gedges[gindex[v+1]] );
+		    if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
+			a.finalise();
+		    assert( a.size() == gindex[v+1] - gindex[v] );
+		    assert( a.size() == index[v+1] - index[v] );
+		}
 	    }
 	    if constexpr ( dual_rep ) {
 		std::copy( &gedges[gindex[v]], &gedges[gindex[v+1]],
@@ -535,6 +384,8 @@ public:
     // HGraphTy: some kind of hashed graph
     // This constructor constructs a "PSet" only, i.e., does not distinguish
     // X from P vertices and avoid X-X edges.
+    //
+    // Note: we only use lazy hashing for the full graph
     template<typename CSxGraphTy, typename HGraphTy>
     explicit GraphHAdjPA( const CSxGraphTy & G,
 			  const HGraphTy & H,
@@ -607,7 +458,8 @@ public:
 		// Remap values: XP[i] -> i, same as hash table mapping
 		hash_pa_insert_iterator<dual_rep,true,hash_set_type>
 		    out( a, &hashes[s_next], XP );
-		m_index[ce] = s_next;
+		if constexpr ( has_dual_rep )
+		    m_index[su] = s_next;
 		graptor::set_operations<graptor::hash_scalar>::intersect_ds(
 		    u_adj, XP_slice, out );
 		if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
@@ -616,13 +468,19 @@ public:
 	    }
 	}
 
-	if constexpr ( !preallocate_storage )
+	if constexpr ( !preallocate_storage && has_dual_rep )
 	    m_index[ce] = s_next;
     }
 
     GraphHAdjPA( const GraphHAdjPA & ) = delete;
 
     ~GraphHAdjPA() {
+	if( !preallocate_storage ) {
+	    for( VID v=0; v < m_n; ++v )
+		// Do not use get_adjacency() as it will needlessly create
+		// the hash set.
+		m_adjacency[v].~hash_set_type();
+	}
 	m_adjacency.del();
 	if constexpr ( preallocate_storage || has_dual_rep )
 	    m_storage.del();
@@ -632,7 +490,13 @@ public:
 
     VID numVertices() const { return m_n; }
     VID get_num_vertices() const { return m_n; }
-    VID getDegree( VID v ) const { return get_adjacency( v ).size(); }
+    VID get_degree( VID v ) const {
+	if constexpr ( lazy_hashing )
+	    return m_index[v+1] - m_index[v];
+	else
+	    return get_adjacency( v ).size();
+    }
+    VID getDegree( VID v ) const { return get_degree( v ); }
     VID get_right_degree( VID v ) const {
 	auto pos = std::lower_bound( nbegin( v ), nend( v ), v );
 	return std::distance( pos, nend( v ) );
@@ -646,7 +510,32 @@ public:
     hash_set_type & get_adjacency( VID v ) const {
 	return const_cast<self_type *>( this )->get_adjacency( v );
     }
-    hash_set_type & get_adjacency( VID v ) { return m_adjacency[v]; }
+    hash_set_type & get_adjacency( VID v ) {
+	hash_set_type & a = m_adjacency[v];
+	if constexpr ( lazy_hashing ) {
+	    // Relies on a linearisation order where an element is added
+	    // before the size counter is incremented.
+	    // This does not work correctly for delta hashing which requires
+	    // an additional finalisation stage.
+	    if( a.size() != m_index[v+1] - m_index[v] ) {
+		std::lock_guard<std::mutex> guard( a.get_lock() );
+		if( a.size() == 0 ) {
+		    a.insert( &m_storage[m_index[v]], &m_storage[m_index[v+1]] );
+		    if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
+			a.finalise();
+		    assert( a.size() == m_index[v+1] - m_index[v] );
+		}
+	    }
+	}
+	return a;
+    }
+
+    bool is_adjacency_initialised( VID v ) const {
+	if constexpr ( lazy_hashing )
+	    return m_adjacency[v].size() !=0 || m_index[v+1] == m_index[v];
+	else
+	    return true;
+    }
 
     const VID * get_neighbours( VID v ) const {
 	if constexpr ( dual_rep ) {
@@ -658,9 +547,15 @@ public:
 	    return nullptr;
     }
 
+    auto get_lazy_neighbours_set( VID v ) const {
+	if constexpr ( lazy_hashing )
+	    return ngh_set_type( get_neighbours( v ), get_degree( v ) );
+	else
+	    return get_neighbours_set( v );
+    }
     dual_set_type get_neighbours_set( VID v ) const {
 	return dual_set_type(
-	    ngh_set_type( get_neighbours( v ), getDegree( v ) ),
+	    ngh_set_type( get_neighbours( v ), get_degree( v ) ),
 	    get_adjacency( v ) );
     }
     dual_set_type get_left_neighbours_set( VID v ) const {
@@ -675,6 +570,12 @@ public:
 	auto r = std::upper_bound( b, e, v );
 	return dual_set_type( ngh_set_type( r, e ), get_adjacency( v ) );
     }
+    size_t get_num_right_neighbours( VID v ) const {
+	auto b = nbegin( v );
+	auto e = nend( v );
+	auto r = std::upper_bound( b, e, v );
+	return std::distance( r, e );
+    }
 
     neighbour_iterator nbegin( VID v ) {
 	return get_neighbours( v );
@@ -683,10 +584,10 @@ public:
 	return get_neighbours( v );
     }
     neighbour_iterator nend( VID v ) {
-	return get_neighbours( v ) + getDegree( v );
+	return get_neighbours( v ) + get_degree( v );
     }
     const_neighbour_iterator nend( VID v ) const {
-	return get_neighbours( v ) + getDegree( v );
+	return get_neighbours( v ) + get_degree( v );
     }
 
 /*
@@ -722,6 +623,182 @@ private:
     VID m_n;
     mm::buffer<VID> m_storage;
     mm::buffer<EID> m_index;
+    mm::buffer<hash_set_type> m_adjacency;
+};
+
+template<typename lVID, typename lEID,
+	 bool lazyhash,
+	 typename lHashSet>
+class GraphLazyHashedAdj {
+public:
+    using self_type =
+	GraphLazyHashedAdj<lVID,lEID,lazyhash,lHashSet>;
+    using VID = lVID;
+    using EID = lEID;
+    using hash_set_type = lHashSet;
+    using Hash = typename hash_set_type::hash_type;
+    using ngh_set_type = graptor::array_slice<VID,VID>;
+    using dual_set_type = graptor::dual_set<ngh_set_type,hash_set_type>;
+
+    using vertex_iterator = range_iterator<VID>;
+    using edge_iterator = generic_edge_iterator<VID,EID>;
+    using neighbour_iterator = VID *;
+    using const_neighbour_iterator = const VID *;
+
+    static constexpr bool has_dual_rep = true;
+    static constexpr bool preallocate_storage = false;
+    static constexpr bool lazy_hashing = lazyhash;
+
+    static_assert( !preallocate_storage || !lazy_hashing,
+		   "Lazy hashing implemented only for non-preallocated "
+		   "storage" );
+
+public:
+    explicit GraphLazyHashedAdj( const ::GraphCSx & G,
+				 numa_allocation && alloc,
+				 size_t lazy_threshold = 16 ) :
+	m_graph( G ),
+	m_adjacency( G.numVertices(), alloc ) {
+
+	VID n = m_graph.get_num_vertices();
+	const EID * const gindex = G.getIndex();
+	const VID * const gedges = G.getEdges();
+
+	parallel_loop( VID(0), n, [&]( VID v ) {
+	    EID deg = gindex[v+1] - gindex[v];
+	    hash_set_type & a = m_adjacency[v];
+	    if( lazy_hashing && deg < lazy_threshold ) {
+		new ( &a ) hash_set_type(); // size hint empty
+	    } else {
+		new ( &a ) hash_set_type( deg ); // size hint
+		a.insert( &gedges[gindex[v]], &gedges[gindex[v+1]] );
+		if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
+		    a.finalise();
+		assert( a.size() == gindex[v+1] - gindex[v] );
+	    }
+	} );
+    }
+
+    GraphLazyHashedAdj( const GraphLazyHashedAdj & ) = delete;
+
+    ~GraphLazyHashedAdj() {
+	VID n = m_graph.get_num_vertices();
+	for( VID v=0; v < n; ++v )
+	    // Do not use get_adjacency() as it will needlessly create
+	    // the hash set.
+	    m_adjacency[v].~hash_set_type();
+	m_adjacency.del();
+    }
+
+    VID numVertices() const { return m_graph.get_num_vertices(); }
+    VID get_num_vertices() const { return m_graph.get_num_vertices(); }
+    VID get_degree( VID v ) const { return m_graph.get_degree( v ); }
+    VID getDegree( VID v ) const { return get_degree( v ); }
+    VID get_right_degree( VID v ) const {
+	auto pos = std::lower_bound( nbegin( v ), nend( v ), v );
+	return std::distance( pos, nend( v ) );
+    }
+
+    vertex_iterator vbegin() { return vertex_iterator( 0 ); }
+    vertex_iterator vbegin() const { return vertex_iterator( 0 ); }
+    vertex_iterator vend() { return vertex_iterator( get_num_vertices() ); }
+    vertex_iterator vend() const { return vertex_iterator( get_num_vertices() ); }
+
+    hash_set_type & get_adjacency( VID v ) const {
+	return const_cast<self_type *>( this )->get_adjacency( v );
+    }
+    hash_set_type & get_adjacency( VID v ) {
+	hash_set_type & a = m_adjacency[v];
+	if constexpr ( lazy_hashing ) {
+	    // Relies on a linearisation order where an element is added
+	    // before the size counter is incremented.
+	    // This does not work correctly for delta hashing which requires
+	    // an additional finalisation stage.
+	    if( a.size() != get_degree( v ) ) {
+		std::lock_guard<std::mutex> guard( a.get_lock() );
+		if( a.size() == 0 ) {
+		    a.insert( m_graph.get_neighbours( v ),
+			      m_graph.get_neighbours( v ) + get_degree( v ) );
+		    if constexpr ( graptor::is_hash_set_hopscotch_delta_v<hash_set_type> )
+			a.finalise();
+		    assert( a.size() == get_degree( v ) );
+		}
+	    }
+	}
+	return a;
+    }
+
+    bool is_adjacency_initialised( VID v ) const {
+	if constexpr ( lazy_hashing )
+	    return m_adjacency[v].size() == get_degree( v );
+	else
+	    return true;
+    }
+
+    const VID * get_neighbours( VID v ) const {
+	return m_graph.get_neighbours( v );
+    }
+
+    auto get_lazy_neighbours_set( VID v ) const {
+	if constexpr ( lazy_hashing )
+	    return ngh_set_type( get_neighbours( v ), get_degree( v ) );
+	else
+	    return get_neighbours_set( v );
+    }
+    dual_set_type get_neighbours_set( VID v ) const {
+	return dual_set_type(
+	    ngh_set_type( get_neighbours( v ), get_degree( v ) ),
+	    get_adjacency( v ) );
+    }
+    dual_set_type get_left_neighbours_set( VID v ) const {
+	auto b = nbegin( v );
+	auto e = nend( v );
+	auto r = std::lower_bound( b, e, v );
+	return dual_set_type( ngh_set_type( b, r ), get_adjacency( v ) );
+    }
+    dual_set_type get_right_neighbours_set( VID v ) const {
+	auto b = nbegin( v );
+	auto e = nend( v );
+	auto r = std::upper_bound( b, e, v );
+	return dual_set_type( ngh_set_type( r, e ), get_adjacency( v ) );
+    }
+    size_t get_num_right_neighbours( VID v ) const {
+	auto b = nbegin( v );
+	auto e = nend( v );
+	auto r = std::upper_bound( b, e, v );
+	return std::distance( r, e );
+    }
+
+    neighbour_iterator nbegin( VID v ) {
+	return get_neighbours( v );
+    }
+    const_neighbour_iterator nbegin( VID v ) const {
+	return get_neighbours( v );
+    }
+    neighbour_iterator nend( VID v ) {
+	return get_neighbours( v ) + get_degree( v );
+    }
+    const_neighbour_iterator nend( VID v ) const {
+	return get_neighbours( v ) + get_degree( v );
+    }
+
+/*
+    edge_iterator ebegin() {
+	return edge_iterator( 0, 0, m_index.get(), m_edges.get() );
+    }
+    edge_iterator ebegin() const {
+	return edge_iterator( 0, 0, m_index.get(), m_edges.get() );
+    }
+    edge_iterator eend() {
+	return edge_iterator( m_n, m_m, m_index.get(), m_edges.get() );
+    }
+    edge_iterator eend() const {
+	return edge_iterator( m_n, m_m, m_index.get(), m_edges.get() );
+    }
+*/
+
+private:
+    const ::GraphCSx & m_graph;
     mm::buffer<hash_set_type> m_adjacency;
 };
 
